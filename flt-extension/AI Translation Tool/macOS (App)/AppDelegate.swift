cf8987608e316d2c773a4d5242022f0755bae050
Timur Sultanov <tssultanov@outlook.com>
2025-04-21 20:47:57 +0400

    Add Sfari extension project

---
+++ flt-extension/AI Translation Tool/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Timur Sultanov on 21.04.25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
