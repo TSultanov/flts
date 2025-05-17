a26e4a1e74319c760b984836fb9aeccdc686bd10
Timur Sultanov <tssultanov@outlook.com>
2025-05-17 20:47:56 +0400

    Initial commit for new viewer

---
+++ site/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { Router, type RouteConfig } from "@mateothegreat/svelte5-router";
+  import type { RouterInstance } from "@mateothegreat/svelte5-router";
+  import Config from "./lib/Config.svelte";
+  import Home from "./lib/Home.svelte";
+  import Nav from "./lib/Nav.svelte";
+
+  const routes: RouteConfig[] = [
+    {
+      name: "Home",
+      component: Home,
+    },
+    {
+      path: "config",
+      name: "Config",
+      component: Config,
+    },
+  ];
+
+  let router = $state<RouterInstance>();
+  let current = $derived(router?.current);
+</script>
+
+<Nav {routes} {current} />
+<Router bind:instance={router} {routes} />
+
+<style>
+</style>
---
+++ site/src/lib/Nav.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { type RouteConfig, type RouteResult } from "@mateothegreat/svelte5-router";
+
+    let { routes, current }: {
+        routes: RouteConfig[],
+        current: RouteResult | undefined,
+    } = $props();
+
+    let currentPath = $derived(current?.result.path.original);
+    $inspect(currentPath);
+</script>
+
+<nav>
+    {#each routes as route }
+    <a
+    href="/{route.path}"
+    class={(route.path?.toString() ?? '') === (currentPath?.toString().substring(1) ?? '') ? 'current' : ''}>{route.name}</a>
+    {/each}
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        background-color: #f2f2f2;
+        border-bottom: 1px solid #555555;
+        padding: 0px 10px 0px 10px;
+    }
+
+    a {
+        display: inline-block;
+        background-color: #333333;
+        color: #f2f2f2;
+        text-decoration: none;
+        padding: 10px;
+    }
+
+    a.current {
+        background-color: #555555;
+    }
+
+    a:hover {
+        background-color: #777777;
+    }
+</style>
