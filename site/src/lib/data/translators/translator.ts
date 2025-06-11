5da07afe0229ce09e45256e5eeecfff3c9b1232f
Timur Sultanov <tssultanov@outlook.com>
2025-06-11 22:20:19 +0400

    Remove depreacated model

--- site/src/lib/data/translators/translator.ts
+++ site/src/lib/data/translators/translator.ts
@@ -46,7 +46,6 @@ export interface Translator {
 
 export type ModelId =
 "gemini-2.5-flash-preview-05-20" |
-"gemini-2.5-pro-preview-05-06" |
 "gemini-2.5-pro-preview-06-05"
 
 export type ModelProvider = "Google"
