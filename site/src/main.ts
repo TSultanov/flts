c1502f9ce72b6d21026efa5642b7db1ddf4d8dfc
Timur Sultanov <tssultanov@outlook.com>
2025-11-18 20:35:50 +0400

    Remove top-level await

--- site/src/main.ts
+++ site/src/main.ts
@@ -3,7 +3,7 @@ import './app.css'
 import App from './App.svelte'
 import { attachConsole } from '@tauri-apps/plugin-log';
 
-await attachConsole();
+attachConsole();
 
 const app = mount(App, {
   target: document.getElementById('app')!,
