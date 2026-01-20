a5209a00a5da818d7febab311340641c9cd16950
Timur Sultanov <tssultanov@outlook.com>
2026-01-20 19:51:28 +0400

    Reduce cache footprint on mobile

--- library/src/cache.rs
+++ library/src/cache.rs
@@ -8,6 +8,18 @@ use isolang::Language;
 
 use crate::book::translation_import::ParagraphTranslation;
 
+const MIB: usize = 1024 * 1024;
+
+#[cfg(any(target_os = "ios", target_os = "android"))]
+const TRANSLATIONS_CACHE_MEMORY_CAPACITY: usize = 32 * MIB;
+#[cfg(not(any(target_os = "ios", target_os = "android")))]
+const TRANSLATIONS_CACHE_MEMORY_CAPACITY: usize = 256 * MIB;
+
+#[cfg(any(target_os = "ios", target_os = "android"))]
+const TRANSLATIONS_CACHE_STORAGE_CAPACITY: usize = 128 * MIB;
+#[cfg(not(any(target_os = "ios", target_os = "android")))]
+const TRANSLATIONS_CACHE_STORAGE_CAPACITY: usize = 1024 * MIB;
+
 pub struct TranslationsCache {
     cache: HybridCache<String, ParagraphTranslation>,
 }
@@ -15,16 +27,29 @@ pub struct TranslationsCache {
 impl TranslationsCache {
     pub async fn create(cache_dir: &Path) -> anyhow::Result<Self> {
         let device = FsDeviceBuilder::new(cache_dir)
-            .with_capacity(1024 * 1024 * 1024)
+            .with_capacity(TRANSLATIONS_CACHE_STORAGE_CAPACITY)
             .build()?;
         let cache = HybridCacheBuilder::new()
             .with_policy(HybridCachePolicy::WriteOnInsertion)
-            .memory(256 * 1024 * 1024)
+            .memory(TRANSLATIONS_CACHE_MEMORY_CAPACITY)
             .storage()
             .with_engine_config(BlockEngineBuilder::new(device))
             .build()
             .await?;
         Ok(Self { cache })
+    }
+
+    fn make_key(
+        source_language: &Language,
+        target_language: &Language,
+        paragraph: &str,
+    ) -> String {
+        format!(
+            "{}\n{}\n{}",
+            source_language.to_639_3(),
+            target_language.to_639_3(),
+            paragraph
+        )
     }
 
     pub fn set(
@@ -35,12 +60,7 @@ impl TranslationsCache {
         data: &ParagraphTranslation,
     ) {
         self.cache.insert(
-            format!(
-                "{}\n{}\n{}",
-                source_language.to_639_3(),
-                target_language.to_639_3(),
-                paragraph.to_owned()
-            ),
+            Self::make_key(source_language, target_language, paragraph),
             data.clone(),
         );
     }
@@ -53,12 +73,7 @@ impl TranslationsCache {
     ) -> anyhow::Result<Option<ParagraphTranslation>> {
         Ok(self
             .cache
-            .get(&format!(
-                "{}\n{}\n{}",
-                source_language.to_639_3(),
-                target_language.to_639_3(),
-                paragraph.to_owned()
-            ))
+            .get(&Self::make_key(source_language, target_language, paragraph))
             .await?
             .map(|r| r.value().clone()))
     }
